0c259d7d0d3d4f094248290249472fbd1dfae12d
João Cintra Silva <joaocintra@hotmail.com>
2018-06-02 15:02:39 +0100

    Add project files.

---
+++ BeerScrapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BeerScrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
